6b24b0e8c2f164d61546f5a53a3ed9036c9adf0a
Andreas Spannagel <spannagel.andreas@gmail.com>
2024-04-14 23:13:35 +0200

    Add answer endpoint

--- workspaces/retrieval/src/namespace/retrieval/retrieval_app/urls.py
+++ workspaces/retrieval/src/namespace/retrieval/retrieval_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("answer/", views.answer, name="answer"),
 ]
--- workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
+++ workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 async def index(request):
     return render(request, "index.html")
+
+
+async def answer(request):
+    user_input = request.POST.get("user-input")
+    answer = "I don't know the answer to that question."
+    
+    return render(request, "answer.html", {"user_input": user_input, "answer": answer})

10902123c42983302856b8b05941d8c24120f61a
Andreas Spannagel <spannagel.andreas@gmail.com>
2024-04-15 01:16:58 +0200

    Add OpenAI API call to view
    Return answer via HTMX (no stream yet)

--- workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
+++ workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
@@ -1,4 +1,12 @@
+from django.http import StreamingHttpResponse
+import httpx
+import openai
 from django.shortcuts import render
+
+OPENAI_MODEL_NAME = "gpt-3.5-turbo-0125"  # gpt-4-turbo-2024-04-09
+MODEL_TEMPERATURE = 0.5
+
+client = None
 
 
 async def index(request):
@@ -6,7 +14,34 @@ async def index(request):
 
 
 async def answer(request):
-    user_input = request.POST.get("user-input")
-    answer = "I don't know the answer to that question."
-    
-    return render(request, "answer.html", {"user_input": user_input, "answer": answer})
+    query = request.POST.get("query")
+
+    openai_client = await get_openai_client()
+    prompt_value = f"You are an expert for AWS cloud services. Please answer the following question(s): {query}"
+
+    response = StreamingHttpResponse(
+        stream_chain(openai_client, prompt_value, OPENAI_MODEL_NAME)
+    )
+
+    return response
+
+
+async def get_openai_client() -> openai.AsyncClient:
+    global client
+    if client is None or client.is_closed:
+        client = httpx.AsyncClient(timeout=120.0, http2=True)
+    return openai.AsyncClient(http_client=client)
+
+
+async def stream_chain(async_client: openai.AsyncClient, prompt_value: str, model: str):
+    messages = [{"role": "user", "content": prompt_value}]
+    stream = await async_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        stream=True,
+        temperature=MODEL_TEMPERATURE,
+    )
+
+    async for chunk in stream:
+        if chunk.choices:
+            yield chunk.choices[0].delta.content  # .encode('utf-8')?

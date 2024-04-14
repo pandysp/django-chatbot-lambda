d91d783ae54a2450820df86237ee3aef581574f9
Andreas Spannagel <spannagel.andreas@gmail.com>
2024-04-14 22:08:02 +0200

    Add basic chatbot user interface

--- workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
+++ workspaces/retrieval/src/namespace/retrieval/retrieval_app/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+async def index(request):
+    return render(request, "index.html")
